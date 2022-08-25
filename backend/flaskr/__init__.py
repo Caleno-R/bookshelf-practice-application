83981e9b3a5d11ea1f4e5ebe27267c72ad437c50
Caleno-R <calenorose@gmail.com>
2022-08-25 08:55:04 +0300

    retrive books, paginated done

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -32,12 +32,17 @@ def create_app(test_config=None):
         )
         return response
 
-    # @TODO: Write a route that retrivies all books, paginated.
-    #         You can use the constant above to paginate by eight books.
-    #         If you decide to change the number of books per page,
-    #         update the frontend to handle additional books in the styling and pagination
-    #         Response body keys: 'success', 'books' and 'total_books'
-    # TEST: When completed, the webpage will display books including title, author, and rating shown as stars
+ 
+    def paginate_books(request,selection):
+        page = request.args.get('page', 1, type=int)
+        start = (page - 1) * BOOKS_PER_SHELF
+        end = start + BOOKS_PER_SHELF
+
+        books = [book.format() for book in selection]
+        current_books = books[start:end]
+
+        return current_books
+
 
     # @TODO: Write a route that will update a single book's rating.
     #         It should only be able to update the rating, not the entire representation
@@ -55,5 +60,21 @@ def create_app(test_config=None):
     #        Response body keys: 'success', 'created'(id of created book), 'books' and 'total_books'
     # TEST: When completed, you will be able to a new book using the form. Try doing so from the last page of books.
     #       Your new book should show up immediately after you submit it at the end of the page.
+
+    @app.route('/books')
+    def retrieve_books():
+        selection = Book.query.order_by('Book.id').all()
+        current_books = paginate_books(request, selection)
+
+        if len(current_books) == 0:
+            abort(404)
+            
+        return jsonify({
+            'success':True,
+            'books':current_books,
+            'total_books': len(Book.query.all())
+        })
+
+
 
     return app
